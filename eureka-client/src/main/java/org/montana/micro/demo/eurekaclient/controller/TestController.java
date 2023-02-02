0b737cacf44265ac7586c3ad67a988c6660cdde1
Ihor Sukalin <naslednikg@gmail.com>
2023-02-02 17:23:31 +0200

    add gateway routing for eclient mc

--- eureka-client/src/main/java/org/montana/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/org/montana/micro/demo/eurekaclient/controller/TestController.java
@@ -1,6 +1,7 @@
 package org.montana.micro.demo.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
